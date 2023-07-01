fffcb56283184512cde6318f9b0fa1963f9f3a68
Spyros Siannas <ssiannas@meta.com>
2023-07-01 03:38:52 +0300

    Add gitignore

--- build/CMakeFiles/chesster.dir/chess-engine/Board.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/chesster.dir/chess-engine/Board.cpp.o: \
-  /Users/ssiannas/code_proj/chesster/chess-engine/Board.cpp \
-  /Users/ssiannas/code_proj/chesster/chess-engine/Board.hpp
--- build/chess-engine/CMakeFiles/chesster.dir/Board.cpp.o.d
+++
@@ -1,3 +0,0 @@
-chess-engine/CMakeFiles/chesster.dir/Board.cpp.o: \
-  /Users/ssiannas/code_proj/chesster/chess-engine/Board.cpp \
-  /Users/ssiannas/code_proj/chesster/chess-engine/Board.hpp
