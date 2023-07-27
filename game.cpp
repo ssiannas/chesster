129887bf00fb7783d4e074a5b23deb8b65075f27
SpyrosSiannas <ssiannas@gmail.com>
2023-07-27 03:14:12 +0300

    Be a cool kid 😎

--- game.cpp
+++ game.cpp
@@ -1,7 +1,7 @@
 #include <Board.hpp>
 #include <iostream>
 
-int main() {
+auto main() -> int {
   auto b = chesster::Board();
   b.positionFromFEN(
       "rnbqkbnr/pp1ppppp/8/2p5/4P3/5N2/PPPP1PPP/RNBQKB1R b KQkq - 1 2");
